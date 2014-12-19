d590da81ec8721e40e58103b2748fca4f84b21cd
Nefarius <nefarius@dhmx.at>
2014-12-19 21:46:22 +0100

    Added LimitInstance class

---
+++ Libarius/System/LimitInstance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Libarius.System
+{
+    public class LimitInstance
+    {
+        private string instanceName = string.Empty;
+        private Mutex mutex;
+
+        public LimitInstance(string instanceName)
+        {
+            this.instanceName = instanceName;
+        }
+
+        public void Dispose()
+        {
+            if (mutex != null)
+            {
+                mutex.Close();
+            }
+        }
+
+        public bool IsOnlyInstance
+        {
+            get
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(instanceName);
+                    return false;
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    mutex = new Mutex(true, instanceName);
+                    return true;
+                }
+            }
+        }
+    }
+}
