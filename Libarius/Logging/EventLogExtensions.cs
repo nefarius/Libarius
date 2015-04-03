8b6d4179a40b461a584a08c28ea74efea8815000
Nefarius <nefarius@dhmx.at>
2015-04-03 09:45:35 +0200

    Updated version Added EventLogExtentions Added Filesystemhelper Added BindingHelper

---
+++ Libarius/Logging/EventLogExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Libarius.Logging
+{
+    public static class EventLogExtensions
+    {
+        public static void WriteInformation(this EventLog log, string format, params object[] objs)
+        {
+            log.WriteEntry(string.Format(format, objs), EventLogEntryType.Information);
+        }
+
+        public static void WriteError(this EventLog log, string format, params object[] objs)
+        {
+            log.WriteEntry(string.Format(format, objs), EventLogEntryType.Error);
+        }
+
+        public static void WriteWarning(this EventLog log, string format, params object[] objs)
+        {
+            log.WriteEntry(string.Format(format, objs), EventLogEntryType.Warning);
+        }
+    }
+}
---
+++ Libarius/WPF/BindingHelper.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Libarius.WPF
+{
+    public static class BindingHelper
+    {
+        public static IEnumerable<DependencyObject> EnumerateVisualChildren(this DependencyObject dependencyObject)
+        {
+            for (var i = 0; i < VisualTreeHelper.GetChildrenCount(dependencyObject); i++)
+            {
+                yield return VisualTreeHelper.GetChild(dependencyObject, i);
+            }
+        }
+
+        public static IEnumerable<DependencyObject> EnumerateVisualDescendents(this DependencyObject dependencyObject)
+        {
+            yield return dependencyObject;
+
+            foreach (var child in dependencyObject.EnumerateVisualChildren())
+            {
+                foreach (var descendent in child.EnumerateVisualDescendents())
+                {
+                    yield return descendent;
+                }
+            }
+        }
+
+        public static void UpdateBindingSources(this DependencyObject dependencyObject)
+        {
+            foreach (var element in dependencyObject.EnumerateVisualDescendents())
+            {
+                var localValueEnumerator = element.GetLocalValueEnumerator();
+                while (localValueEnumerator.MoveNext())
+                {
+                    var bindingExpression = BindingOperations.GetBindingExpressionBase(element,
+                        localValueEnumerator.Current.Property);
+                    if (bindingExpression != null)
+                    {
+                        bindingExpression.UpdateSource();
+                    }
+                }
+            }
+        }
+
+        public static void UpdateBindingSources(this Application application)
+        {
+            foreach (Window window in application.Windows)
+            {
+                window.UpdateBindingSources();
+            }
+        }
+    }
+}
