e453ec9cbdbf3e2d9338941079a6e7f8c77f2848
Nefarius <nefarius@dhmx.at>
2015-04-10 14:29:29 +0200

    Added more Active Directory helper methods

--- Libarius/Active Directory/ADHelper.cs
+++ Libarius/Active Directory/ADHelper.cs
@@ -168,5 +168,272 @@ namespace Libarius.Active_Directory
         {
             return (GetGroups(userName).Find(group => group.Guid == gUid) != null);
         }
+
+        public enum ObjectClass
+        {
+            User,
+            Group,
+            Computer
+        }
+
+        public enum ReturnType
+        {
+            DistinguishedName,
+            ObjectGuid
+        }
+
+        /// <summary>
+        /// Searches for provided object in directory and resolves it to distinguished name.
+        /// </summary>
+        /// <param name="objectCls">The object class to search for.</param>
+        /// <param name="returnValue">The format of the returned string.</param>
+        /// <param name="objectName">The common name of the object to search for.</param>
+        /// <param name="ldapDomain">The LDAP domain to search in.</param>
+        /// <returns></returns>
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static string GetObjectDistinguishedName(ObjectClass objectCls,
+            ReturnType returnValue, string objectName, string ldapDomain)
+        {
+            var distinguishedName = string.Empty;
+            var connectionPrefix = string.Format("LDAP://{0}", ldapDomain);
+
+            using (var entry = new DirectoryEntry(connectionPrefix))
+            {
+                using (var mySearcher = new DirectorySearcher(entry))
+                {
+                    switch (objectCls)
+                    {
+                        case ObjectClass.User:
+                            mySearcher.Filter = string.Format("(&(objectClass=user)(|(cn={0})(sAMAccountName={0})))",
+                                objectName);
+                            break;
+                        case ObjectClass.Group:
+                            mySearcher.Filter = string.Format("(&(objectClass=group)(|(cn={0})(dn={0})))", objectName);
+                            break;
+                        case ObjectClass.Computer:
+                            mySearcher.Filter = string.Format("(&(objectClass=computer)(|(cn={0})(dn={0})))", objectName);
+                            break;
+                    }
+
+                    var result = mySearcher.FindOne();
+
+                    if (result == null)
+                    {
+                        throw new NullReferenceException
+                            ("unable to locate the distinguishedName for the object " +
+                             objectName + " in the " + ldapDomain + " domain");
+                    }
+
+                    var directoryObject = result.GetDirectoryEntry();
+
+                    if (returnValue.Equals(ReturnType.DistinguishedName))
+                    {
+                        distinguishedName = string.Format("LDAP://{0}", directoryObject.Properties
+                            ["distinguishedName"].Value);
+                    }
+
+                    if (returnValue.Equals(ReturnType.ObjectGuid))
+                    {
+                        distinguishedName = directoryObject.Guid.ToString();
+                    }
+                }
+            }
+            return distinguishedName;
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static string ConvertDNtoGuid(string objectDn)
+        {
+            //Removed logic to check existence first
+
+            using (var directoryObject = new DirectoryEntry(objectDn))
+            {
+                return directoryObject.Guid.ToString();
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static string ConvertGuidToOctectString(string objectGuid)
+        {
+            var guid = new Guid(objectGuid);
+            var byteGuid = guid.ToByteArray();
+            var queryGuid = "";
+            foreach (var b in byteGuid)
+            {
+                queryGuid += @"\" + b.ToString("x2");
+            }
+            return queryGuid;
+        }
+
+        /// <remarks>Currently broken!</remarks>
+        public static string ConvertGuidToDn(string GUID)
+        {
+            using (var ent = new DirectoryEntry())
+            {
+                var adGuid = ent.NativeGuid;
+                var x = new DirectoryEntry("LDAP://{GUID=" + adGuid + ">");
+                //change the { to <>
+
+                return x.Path.Remove(0, 7); //remove the LDAP prefix from the path
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static void EnableUserAccount(string userDn)
+        {
+            try
+            {
+                using (var user = new DirectoryEntry(userDn))
+                {
+                    var val = (int)user.Properties["userAccountControl"].Value;
+                    user.Properties["userAccountControl"].Value = val & ~0x2; //ADS_UF_NORMAL_ACCOUNT;
+
+                    user.CommitChanges();
+                }
+            }
+            catch (DirectoryServicesCOMException)
+            {
+                //TODO: implement proper error handling
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static void DisableUserAccount(string userDn)
+        {
+            try
+            {
+                using (var user = new DirectoryEntry(userDn))
+                {
+                    var val = (int)user.Properties["userAccountControl"].Value;
+                    user.Properties["userAccountControl"].Value = val | 0x2; //ADS_UF_ACCOUNTDISABLE;
+
+                    user.CommitChanges();
+                }
+            }
+            catch (DirectoryServicesCOMException)
+            {
+                //TODO: implement proper error handling
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static void Unlock(string userDn)
+        {
+            try
+            {
+                using (var uEntry = new DirectoryEntry(userDn))
+                {
+                    uEntry.Properties["LockOutTime"].Value = 0; //unlock account
+                    uEntry.CommitChanges();
+                }
+            }
+            catch (DirectoryServicesCOMException)
+            {
+                //TODO: implement proper error handling
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static void ResetPassword(string userDn, string password)
+        {
+            using (var uEntry = new DirectoryEntry(userDn))
+            {
+                uEntry.Invoke("SetPassword", password);
+                uEntry.Properties["LockOutTime"].Value = 0; //unlock account
+                uEntry.CommitChanges();
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static string CreateUserAccount(string ldapPath, string userName, string userPassword)
+        {
+            var oGUID = string.Empty;
+
+            try
+            {
+                var connectionPrefix = string.Format("LDAP://{0}", ldapPath);
+
+                using (var dirEntry = new DirectoryEntry(connectionPrefix))
+                {
+                    var newUser = dirEntry.Children.Add(string.Format("CN={0}", userName), "user");
+                    newUser.Properties["samAccountName"].Value = userName;
+                    newUser.CommitChanges();
+                    oGUID = newUser.Guid.ToString();
+
+                    newUser.Invoke("SetPassword", userPassword);
+                    newUser.CommitChanges();
+                    newUser.Close();
+                }
+            }
+            catch (DirectoryServicesCOMException)
+            {
+                //TODO: implement proper error handling
+            }
+            return oGUID;
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        private static IEnumerable<string> AttributeValuesMultiString(string attributeName, string objectDn,
+            List<string> valuesCollection, bool recursive)
+        {
+            using (var ent = new DirectoryEntry(objectDn))
+            {
+                var valueCollection = ent.Properties[attributeName];
+                var en = valueCollection.GetEnumerator();
+
+                while (en.MoveNext())
+                {
+                    if (en.Current != null)
+                    {
+                        if (!valuesCollection.Contains(en.Current.ToString()))
+                        {
+                            valuesCollection.Add(en.Current.ToString());
+                            if (recursive)
+                            {
+                                AttributeValuesMultiString(attributeName, string.Format("LDAP://{0}",
+                                    en.Current), valuesCollection, true);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return valuesCollection;
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static string AttributeValuesSingleString
+            (string attributeName, string objectDn)
+        {
+            using (var ent = new DirectoryEntry(objectDn))
+            {
+                return ent.Properties[attributeName].Value.ToString();
+            }
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static IEnumerable<string> Groups(string userDn, bool recursive)
+        {
+            var groupMemberships = new List<string>();
+            return AttributeValuesMultiString("memberOf", userDn,
+                groupMemberships, recursive);
+        }
+
+        /// <remarks>http://www.codeproject.com/Articles/18102/Howto-Almost-Everything-In-Active-Directory-via-C</remarks>
+        public static void AddToGroup(string userDn, string groupDn)
+        {
+            try
+            {
+                using (var dirEntry = new DirectoryEntry(string.Format("LDAP://{0}", groupDn)))
+                {
+                    dirEntry.Properties["member"].Add(userDn);
+                    dirEntry.CommitChanges();
+                }
+            }
+            catch (DirectoryServicesCOMException)
+            {
+                //TODO: implement proper error handling
+            }
+        }
     }
 }
--- Libarius/Properties/AssemblyInfo.cs
+++ Libarius/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Benjamin Höglinger")]
 [assembly: AssemblyProduct("Libarius")]
-[assembly: AssemblyCopyright("Copyright © Benjamin Höglinger 2014")]
+[assembly: AssemblyCopyright("Copyright © Benjamin Höglinger 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.1.0")]
-[assembly: AssemblyFileVersion("1.10.1.0")]
+[assembly: AssemblyVersion("1.11.0.0")]
+[assembly: AssemblyFileVersion("1.11.0.0")]
