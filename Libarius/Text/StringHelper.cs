0cf4625d339bbd3cc71c135e4855ed92ce803f9b
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-07 21:38:08 +0200

    Added StringHelper

---
+++ Libarius/Text/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace Libarius.Text
+{
+    /// <summary>
+    ///     Provides utility methods for string interaction.
+    /// </summary>
+    public static class StringHelper
+    {
+        /// <summary>
+        ///     Converts a string of foreign encoding to UTF-8.
+        /// </summary>
+        /// <param name="input">The source string.</param>
+        /// <returns>The UTF-8 encoded string.</returns>
+        public static string ToUtf8(this string input)
+        {
+            var utf8Bytes = Encoding.UTF8.GetBytes(input);
+            return Encoding.UTF8.GetString(utf8Bytes);
+        }
+    }
+}
