b9b31841acf702bcfa2decb36dd96f80f52be056
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-07 21:41:03 +0200

    Added ExceptionExtensions

---
+++ Libarius/Core/ExceptionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Libarius.Core
+{
+    public static class ExceptionExtensions
+    {
+        // all error checking left out for brevity
+
+        // a.k.a., linked list style enumerator
+        private static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem,
+            Func<TSource, bool> canContinue)
+        {
+            for (var current = source; canContinue(current); current = nextItem(current))
+            {
+                yield return current;
+            }
+        }
+
+        private static IEnumerable<TSource> FromHierarchy<TSource>(
+            this TSource source,
+            Func<TSource, TSource> nextItem)
+            where TSource : class
+        {
+            return FromHierarchy(source, nextItem, s => s != null);
+        }
+
+        public static string GetaAllMessages(this Exception exception)
+        {
+            var messages = exception.FromHierarchy(ex => ex.InnerException)
+                .Select(ex => ex.Message);
+            return String.Join(Environment.NewLine, messages);
+        }
+    }
+}
